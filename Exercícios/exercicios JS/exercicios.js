2d70bc94172215f66ec6465fc319a40e93dced30
jordan <jordansnatos@gmail.com>
2026-05-25 10:37:15 -0300

    exercicios feitos até o 10

---
+++ Exercícios/exercicios JS/exercicios.js
@@ -0,0 +1,123 @@
+// Ex 01 calculadora simples
+// let n1 = 20;
+// let n2 = 30;
+
+// let operacao = prompt("Digite a operação que deseja (+ , - , * , / )");
+
+// if(operacao === "+"){
+//     let soma = n1 + n2
+//     console.log(soma);
+// }else if(operacao === "-"){
+//     let sub = n1 - n2
+//     console.log(sub);
+// }else if(operacao === "/"){
+//     let Div = n1 / n2
+//     console.log(Div);
+// }else if (operacao === "*"){
+//     let multi = n1 * n2
+//     console.log(multi);
+// }else{
+//     console.log("Operação inválida")
+// }
+// ------------------------------------------
+
+// Ex 02 Resto da Divisão
+
+// let number = Number(prompt("Digite um numero: "));
+
+// if(number % 2 == 0){
+//     console.log("Par");
+// }else{
+//     console.log("Ímpar")
+// }
+// -----------------------------------------------
+// Ex 03
+// let quadrado = number ** 2;
+// let cubo =  number ** 3;
+
+// console.log("Quadrado: ", quadrado);
+// console.log("Cubo: ", cubo);
+
+// ------------------------------------------------
+
+// Ex 04
+
+// let C = 25;
+// let F = (C * 9/5) + 32;
+
+// console.log(F)
+
+// -------------------------------------------------
+
+//  Ex 05
+// let nota1 = 6.5;
+// let nota2 = 8;
+// let nota3 = 5;
+
+// media = (nota1 + nota2 + nota3) / 3;
+
+// console.log(media.toFixed(2));
+// -----------------------------------------------
+
+// Ex 06
+
+// let valorCompra = 50;
+// let valorPago = 70;
+
+// valorTroco = (valorPago - valorCompra);
+
+// console.log(` Seu troco é: ${valorTroco}`);
+// -------------------------------------------------
+
+// Ex 07
+// let number1 = 10;
+// let number2 = "10";
+
+// console.log(number1 == number2);
+// == compara sommente os valores por isso ele fica true
+
+// console.log(number1 === number2);
+// compara os valores e os tipos por isso false
+// -------------------------------------------------
+
+// Ex 08
+
+// let n1 = 10;
+// let n2 = 5;
+
+// if(n1 > n2){
+//     console.log(` o maior é: ${n1}`)
+// }else{
+//     console.log(`o maior é: ${n2}`)
+// }
+
+// ----------------------------------------------------
+
+// Ex 09
+
+// let nota1 = 7.4;
+// let nota2 = 8.5;
+// let nota3 = 5.3;
+
+// let media = (nota1 + nota2 + nota3) / 3;
+
+// if(media >= 7){
+//     console.log(`Aprovado com média: ${media.toFixed(2)}`);
+// }else{
+//     console.log(`Reprovado com média: ${media.toFixed(2)}`);
+// }
+// --------------------------------------------------------------
+
+// Ex 10
+
+// let idade = Number(prompt("Digite sua idade: "));
+
+// if(idade  >= 18){
+//     console.log("maior de idade: " , idade);
+// }else{
+//     console.log("Menor de idade: ", idade);
+// }
+// ----------------------------------------------------------
+
+// Ex 11
+
