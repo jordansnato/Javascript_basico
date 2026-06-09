32e9913c7020eaa0c5a96abaa7faa2f9916ca2c8
Jordan Lemos dos Santos <jordansnatos@gmail.com>
2026-06-09 17:22:33 -0300

    resolução de exercicios

---
+++ Exercícios/exercicios JS/exerciciosAula04/script.js
@@ -0,0 +1,78 @@
+// Ex 01
+
+// let n1 = Number(prompt("digite um numero: "))
+// let n2 = Number(prompt("digite um outro numero: "))
+
+// if(n1 > n2){
+//     alert(`${n1} é o maior`)
+// }else{
+//     alert(`${n2} é o maior`)
+// }
+
+// EX 02
+//  let n1 = Number(prompt("digite um numero: "))
+
+//  if(n1 > 0){
+//     alert(`${n1} é possitivo`)
+//  }else{
+//     alert(`${n1} é negativo`)
+//  }
+
+// Ex 03
+// let letra = prompt("Digite uma letra: ").toLowerCase()
+
+// if(letra === "f"){
+//     alert("Feminino")
+// }else if(letra === "m"){
+//     alert("Masculino")
+// }else{
+//     alert("Sexo inválido")
+// }
+
+// EX 04
+// let vogal = prompt("Digite uma letra: ")
+
+// if(vogal == "a" || vogal == "e" || vogal =="i" || vogal == "o" || vogal == "u"){
+//     alert(`${vogal} é vogal`)
+// }else{
+//     alert(`${vogal} não é vogal`)
+// }
+
+// EX 05
+
+// let n1 =  Number(prompt("digite uma nota: "))
+// let n2 =  Number(prompt("digite uma nota: "))
+// let n3 =  Number(prompt("digite uma nota: "))
+
+
+// let media = (n1 + n2 + n3)/3;
+
+// if(media === 10){
+//     alert("Aprovado com distinção")
+// }else if(media ){
+//     alert("aprovado")
+// }else{
+//     alert("Reprovado")
+// }
+
+// EX 06
+// let n1 = 8
+// let n2 = 6
+// let n3 = 7
+
+// if(n1 > n2 && n1 > n3){
+//     alert(`${n1} é o maior`)
+// }else if (n2 > n1 && n2 > n3){
+//     alert(`${n2} é o maior`)
+// }else{
+//     alert(`${n3} é o maior`)
+// }
+
+// EX 07
+let n1 = 8
+let n2 = 6
+let n3 = 7
+
+if(n1 > n2 && n2 > n3){
+
+}
