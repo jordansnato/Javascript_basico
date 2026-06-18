a2dda2ba971781af59212dd61e929408becd8ed3
Jordan Lemos dos Santos <jordansnatos@gmail.com>
2026-06-18 17:19:46 -0300

    atualização do código

--- Aula 05/laços/script.js
+++ Aula 05/laços/script.js
@@ -9,8 +9,96 @@
 //     }
 // }
 
-let opcao = "não";
-while(opcao != "sim"){
-    alert("Olá seja bem vindo!" + opcao)
-    opcao = prompt("Quer parar? 'sim' - 'não'")
-}
+// let opcao = "não";
+// while(opcao != "sim"){
+//     alert("Olá seja bem vindo!" + opcao)
+//     opcao = prompt("Quer parar? 'sim' - 'não'")
+// }
+
+// DESAFIO
+
+// 1
+// for(let n1 = 1; n1 <= 10; n1++){
+//     console.log(n1)
+// }
+
+// 2
+// for(let i = 0; i <= 20; i++){
+//     if(i % 2 === 0){
+//         console.log(i)
+//     }
+// }
+
+// 3
+// for(let i = 5; i >= 0; i--){
+//     console.log(i)
+// }console.log("Lançamento")
+
+// Tabuada 
+// let numero = 7
+
+// for(let i = 1; i <= 10; i++){
+//     console.log(`${numero} X ${i} = ${numero * i}`)
+// }
+
+// Bateria
+// let bateria = 100;
+
+// while(bateria >= 0){
+//     console.log(`Bateria: ${bateria}%`)
+//     bateria -= 20;
+// }
+
+// let senha = "";
+
+// while(senha !== "123"){
+//     senha = prompt("Digite a senha")
+//     alert("senha incorreta")
+// }
+// alert("Senha confirmada com sucesso")
+
+// Desafio 4
+
+// let energia = 100;
+
+// while(energia > 0){
+//     energia -= 10
+//     console.log(energia)
+// }
+
+// let credito = 0;
+
+// do{
+//     console.log("tentando jogar")
+// }while(credito > 0);
+
+// let opcao ;
+
+// do{
+//     opcao = Number(prompt("1 - joga 2 - Ranking 3 - sair"))
+
+//     switch(opcao){
+//         case 1: alert("Jogando")
+//         break;
+//         case 2: alert("Seu ranking é entre os top 10 (5)")
+//         break;
+//         case 3: alert("Saindo")
+//         break
+//         default : alert("Número inválido")
+//     }
+// }while(opcao !==3)
+
+// DESAFIO 5
+// let playlist = [
+//     "musica 1",
+//     "musica 2",
+//     "musica 3"
+// ];
+
+// for(let i = 0; i < playlist.length; i++){
+//     console.log(playlist[i])
+// }
+
+// DESAFIO 6
+
+let filmes
