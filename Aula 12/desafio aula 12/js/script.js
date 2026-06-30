2b66f7932dfb98df6e0d044284fec50b31a09586
Jordan Lemos dos Santos <jordansnatos@gmail.com>
2026-06-30 17:18:22 -0300

    atualização codigo html e js

--- Aula 12/desafio aula 12/js/script.js
+++ Aula 12/desafio aula 12/js/script.js
@@ -1,21 +1,50 @@
+let cadastrados = 0;
 
-function cadastrar(){
+function cadastrar() {
     let filme = document.getElementById("nomeFilme").value;
     let genero = document.getElementById("generoFilme").value;
     let anoFilme = document.getElementById("anoLancamento").value;
 
-    let li = document.createElement("li")
-    li.innerHTML = `Nome: ${filme} | Genêro: ${genero} 
-    | Data lançamento: ${anoFilme} <span onclick= 'deletarTarefa(this)'>❌</span>` 
+    if (filme === '' || genero === '' || anoFilme === '') {
+        alert("Favor preencha todos os campos!");
+        return;
+    }
 
-    document.querySelector("ul").appendChild(li)
+    let li = document.createElement("li");
+    li.innerHTML = `
+        Nome: ${filme} | Gênero: ${genero} | Ano: ${anoFilme}
+        <span onclick="deletarFilme(this)"> ❌ </span>
+    `;
+
+    document.querySelector("ul").appendChild(li);
+
+    cadastrados++;
+    atualizarCadastrados();
 
     document.getElementById("nomeFilme").value = "";
     document.getElementById("generoFilme").value = "";
     document.getElementById("anoLancamento").value = "";
 }
 
-function deletarTarefa(li){
-    li.parentElement.remove(li)
+function deletarFilme(el) {
+    el.parentElement.remove();
+    cadastrados--;
+    atualizarCadastrados();
+}
 
+function atualizarCadastrados() {
+    document.getElementById("cadastro").innerText =
+        `Filmes cadastrados: ${cadastrados}`;
+}
+
+function trocarTema() {
+    document.body.classList.toggle("dark");
+
+    let botao = document.getElementById("tema");
+
+    if (document.body.classList.contains("dark")) {
+        botao.innerHTML = "🌙 Noite";
+    } else {
+        botao.innerHTML = "☀️ Dia";
+    }
 }
